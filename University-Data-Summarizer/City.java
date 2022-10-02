0942650444642a2c84818d861c2ae78c578dfaed
Simon Alejandrino (Sy) <101426052+spmalejandrino@users.noreply.github.com>
2022-10-01 23:38:49 -0400

    Update City.java

--- University-Data-Summarizer/City.java
+++ University-Data-Summarizer/City.java
@@ -1,10 +1,4 @@
 
-/**
- * Write a description of class City here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class City
 {
     private University university1;
